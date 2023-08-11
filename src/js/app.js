5cdf92a29f4219e2aff3a869f467dc05c91a7c54
winta38 <winta38@gmail.com>
2023-08-12 02:21:28 +0500

    Задание готово

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+function destructuring({ special }) {
+    for (let i in special) {
+        const attack = special[i];
+        special[i] = 'description' in attack ? attack : Object.assign(attack, { description: 'Описание недоступно' })
+    }
+    return special;
+};
+
+export default destructuring;
